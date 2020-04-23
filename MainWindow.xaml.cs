c932450cabccc5fe4316be4df879b03ce37d4592
ZjzMisaka <530514257@qq.com>
2020-04-23 19:58:12 +0900

    add features
    1. 增加忽略多余空格, 无视大小写, 正则表达式功能
    2. 没有线程执行时不能点击停止按钮

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.IO;
 using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace CommentChecker
 {
@@ -67,11 +68,13 @@ namespace CommentChecker
             List<string> extensions = tb_extension.Text.Split(' ').ToList();
             bool? isRecursive = cb_recursive.IsChecked;
 
+            btn_stop.IsEnabled = true;
             await Task.Run(() =>
             {
                 FileTraverse(folderPath, extensions, isRecursive);
                 stopFlag = false;
             });
+            btn_stop.IsEnabled = false;
 
             l_info.Content = "";
 
@@ -137,9 +140,13 @@ namespace CommentChecker
 
             tb_errortext.Text = "";
 
+            bool isIgnoreSpace = cb_ignorespace.IsChecked == true ? true : false;
+            bool isTolower = cb_tolower.IsChecked == true ? true : false;
+            bool isRegexp = cb_regexp.IsChecked == true ? true : false;
 
             int ngCount = 0;
 
+            btn_stop.IsEnabled = true;
             await Task.Run(() =>
             {
                 foreach (string fileName in filePathList)
@@ -152,13 +159,14 @@ namespace CommentChecker
                     {
                         l_info.Content = fileName;
                     });
-                    if (!CheckFile(fileName))
+                    if (!CheckFile(fileName, isIgnoreSpace, isTolower, isRegexp))
                     {
                         ++ngCount;
                     }
                 }
                 stopFlag = false;
             });
+            btn_stop.IsEnabled = false;
             if (ngCount == 0)
             {
                 System.Windows.MessageBox.Show("ALL OK");
@@ -169,7 +177,7 @@ namespace CommentChecker
             }
 
         }
-        private bool CheckFile(string fileName)
+        private bool CheckFile(string fileName, bool isIgnoreSpace, bool isTolower, bool isRegexp)
         {
             int lineNumber;
 
@@ -183,11 +191,45 @@ namespace CommentChecker
 
             string fileContent = System.IO.File.ReadAllText(fileName);
 
+            if (isIgnoreSpace)
+            {
+                fileContent = string.Join(" ", fileContent.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+                startComment = string.Join(" ", startComment.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+                endComment = string.Join(" ", endComment.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            }
+            if (isTolower)
+            {
+                fileContent = fileContent.ToLowerInvariant();
+                startComment = startComment.ToLowerInvariant();
+                endComment = endComment.ToLowerInvariant();
+            }
+
             int startIndex = 0;
             int endIndex = 0;
             int errorIndex = 0;
+            int lastStartIndex = 0;
+            int lastEndIndex = 0;
+            Regex regStart = new Regex(startComment);
+            Regex regEnd = new Regex(endComment);
 
-            if (fileContent.IndexOf(startComment) == -1 && fileContent.IndexOf(endComment) != -1)
+            if (!isRegexp)
+            {
+                startIndex = fileContent.IndexOf(startComment);
+                endIndex = fileContent.IndexOf(endComment);
+                lastStartIndex = fileContent.LastIndexOf(startComment);
+                lastEndIndex = fileContent.LastIndexOf(endComment);
+            }
+            else
+            {
+                MatchCollection mcStart = regStart.Matches(fileContent);
+                MatchCollection mcEnd = regEnd.Matches(fileContent);
+                startIndex = mcStart.Count > 0 ? mcStart[0].Index : -1;
+                endIndex = mcEnd.Count > 0 ? mcEnd[0].Index : -1;
+                lastStartIndex = mcStart.Count > 0 ? mcStart[mcStart.Count - 1].Index : -1;
+                lastEndIndex = mcEnd.Count > 0 ? mcEnd[mcStart.Count - 1].Index : -1;
+            }
+
+            if (startIndex == -1 && endIndex != -1)
             {
                 string currentErrorText = fileName + "\n" + "找不到注释头\n\n";
                 tb_errortext.Dispatcher.Invoke(() =>
@@ -196,7 +238,7 @@ namespace CommentChecker
                 });
                 return false;
             }
-            if (fileContent.IndexOf(endComment) == -1 && fileContent.IndexOf(startComment) != -1)
+            if (endIndex == -1 && startIndex != -1)
             {
                 string currentErrorText = fileName + "\n" + "找不到注释尾\n\n";
                 tb_errortext.Dispatcher.Invoke(() =>
@@ -205,7 +247,7 @@ namespace CommentChecker
                 });
                 return false;
             }
-            if (fileContent.IndexOf(startComment) > fileContent.IndexOf(endComment))
+            if (startIndex > endIndex)
             {
                 startIndex = fileContent.IndexOf(startComment);
                 errorIndex = fileContent.IndexOf(endComment);
@@ -221,15 +263,15 @@ namespace CommentChecker
                 });
                 return false;
             }
-            if (fileContent.LastIndexOf(startComment) > fileContent.LastIndexOf(endComment))
+            if (lastStartIndex > lastEndIndex)
             {
                 errorIndex = fileContent.LastIndexOf(startComment);
-                endIndex = fileContent.LastIndexOf(endComment);
+                lastEndIndex = fileContent.LastIndexOf(endComment);
 
                 string currentErrorText = fileName + "\n";
                 lineNumber = fileContent.Substring(0, errorIndex == -1 ? 0 : errorIndex).Split('\n').Count();
                 currentErrorText += "错误行数: " + lineNumber + "\n";
-                lineNumber = fileContent.Substring(0, endIndex == -1 ? 0 : endIndex).Split('\n').Count();
+                lineNumber = fileContent.Substring(0, lastEndIndex == -1 ? 0 : lastEndIndex).Split('\n').Count();
                 currentErrorText += "结束行数: " + lineNumber + "\n\n";
                 tb_errortext.Dispatcher.Invoke(() =>
                 {
@@ -245,10 +287,26 @@ namespace CommentChecker
             bool startFirst = true;
             while (startIndex < fileContent.Count())
             {
+                if(stopFlag)
+                {
+                    return true;
+                }
+
                 string currentStartComment = startFirst ? startComment : endComment;
                 string currentEndComment = startFirst ? endComment : startComment;
 
-                startIndex = fileContent.IndexOf(currentStartComment, startIndex);
+                regStart = new Regex(startFirst ? startComment : endComment);
+                regEnd = new Regex(startFirst ? endComment : startComment);
+
+                if (!isRegexp)
+                {
+                    startIndex = fileContent.IndexOf(currentStartComment, startIndex);
+                }
+                else
+                {
+                    Match match = regStart.Match(fileContent, startIndex);
+                    startIndex = match.Success ? match.Index : -1;
+                }
 
                 if (startIndex == -1)
                 {
@@ -256,8 +314,18 @@ namespace CommentChecker
                 }
 
                 errorIndex = endIndex = startIndex + 1;
-                endIndex = fileContent.IndexOf(currentEndComment, endIndex);
-                errorIndex = fileContent.IndexOf(currentStartComment, errorIndex);
+                if (!isRegexp)
+                {
+                    endIndex = fileContent.IndexOf(currentEndComment, endIndex);
+                    errorIndex = fileContent.IndexOf(currentStartComment, errorIndex);
+                }
+                else
+                {
+                    Match endMatch = regEnd.Match(fileContent, endIndex);
+                    endIndex = endMatch.Success ? endMatch.Index : -1;
+                    Match errorMatch = regStart.Match(fileContent, errorIndex);
+                    errorIndex = errorMatch.Success ? errorMatch.Index : -1;
+                }
 
                 if (errorIndex != -1 && errorIndex < endIndex)
                 {
